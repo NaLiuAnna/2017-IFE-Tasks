c890474d752ded18f476f7085c8a9c586da58745
DESKTOP-V1IK2ID\auspiciousna <auspiciousna@163.com>
2017-03-14 20:13:13 +0800

    finish

--- task3-4/script/script.js
+++ task3-4/script/script.js
@@ -3,11 +3,11 @@ function execution(){
 	var box = document.getElementById("box");
 	var boxHead = document.getElementById("box_head");
 	var boxBody = document.getElementById("box_body");
+	var headTop = boxHead.getBoundingClientRect().top;
+	var bodyTop = boxBody.getBoundingClientRect().top;
+	var headLeft = boxHead.getBoundingClientRect().left;
+	var bodyLeft = boxBody.getBoundingClientRect().left;
 	if(order == "GO"){
-		var headTop = boxHead.getBoundingClientRect().top;
-		var bodyTop = boxBody.getBoundingClientRect().top;
-		var headLeft = boxHead.getBoundingClientRect().left;
-		var bodyLeft = boxBody.getBoundingClientRect().left;
 		if(headTop < bodyTop){
 			var top = parseInt(box.style.top) -40;
 			if(top > 0){
@@ -31,12 +31,37 @@ function execution(){
 		}
 	}
 	if(order == "TUN LEF"){
-		box.style.transform = "rotate(270deg)";
+		if(headTop < bodyTop){
+			box.style.transform = "rotate(270deg)";
+		}else if(headTop > bodyTop){
+			box.style.transform = "rotate(90deg)";
+		}else if(headLeft < bodyLeft){
+			box.style.transform = "rotate(180deg)";
+		}else{
+			box.style.transform = "rotate(0deg)";
+		}
 	}
 	if(order == "TUN RIG"){
-		box.style.transform = "rotate(90deg)";
-	}	if(order == "TUN BAC"){
-		box.style.transform = "rotate(180deg)";
+		if(headTop < bodyTop){
+			box.style.transform = "rotate(90deg)";
+		}else if(headTop > bodyTop){
+			box.style.transform = "rotate(270deg)";			
+		}else if(headLeft < bodyLeft){
+			box.style.transform = "rotate(0deg)";
+		}else{
+			box.style.transform = "rotate(180deg)";
+		}
+	}
+	if(order == "TUN BAC"){
+		if(headTop < bodyTop){
+			box.style.transform = "rotate(180deg)";
+		}else if(headTop > bodyTop){
+			box.style.transform = "rotate(0deg)";		
+		}else if(headLeft < bodyLeft){
+			box.style.transform = "rotate(90deg)";				
+		}else{
+			box.style.transform = "rotate(270deg)";
+		}
 	}
 }
 
