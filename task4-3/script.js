1f3276b091fe02c89ac9634ec2ff1a36a31d24f8
DESKTOP-V1IK2ID\auspiciousna <auspiciousna@163.com>
2017-03-21 11:33:20 +0800

    change position

--- task4-3/script.js
+++ task4-3/script.js
@@ -2,15 +2,15 @@ var container = document.getElementById('container');
 var clientMenu = document.getElementById('clientMenu');
 
 function menuEvent(event){
-	if(event.clientX + 200 > screen.availWidth){
-		clientMenu.style.left = event.clientX - 200 - 10 + 'px';
+	if(event.clientX + 242 > screen.availWidth){
+		clientMenu.style.left = event.clientX - 242 + 'px';
 	}else{
-		clientMenu.style.left = event.clientX - 10 +'px';
+		clientMenu.style.left = event.clientX + 'px';
 	}
-	if(event.clentY + 80 > screen.availHeight){
-		clientMenu.style.top = event.clientY - 10 - 80 + 'px';
+	if(event.clentY + 122 > screen.availHeight){
+		clientMenu.style.top = event.clientY - 122 + 'px';
 	}else{
-		clientMenu.style.top = event.clientY - 10 + 'px';
+		clientMenu.style.top = event.clientY + 'px';
 	}
 	clientMenu.style.visibility = 'visible';
 	return false;
@@ -20,9 +20,14 @@ function hiddenMenu(){
 	clientMenu.style.visibility = 'hidden';
 }
 
+function defaultMenu(){
+	return false;
+}
+
 function init(){
 	container.oncontextmenu = menuEvent;
 	container.onclick = hiddenMenu;
+	clientMenu.oncontextmenu = defaultMenu;
 }
 
 init();
